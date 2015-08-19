28c9f53f3506c5ec634a205b452177fe1f1d51c1
Zach Garwood <zachgarwood@gmail.com>
2015-08-19 17:11:32 -0500

    Completed batch maintenance script
    PANOPTA-4049

--- panopta-cli/panopta.py
+++ panopta-cli/panopta.py
@@ -1,4 +1,6 @@
-from panopta_rest_api import api_client
+from datetime import datetime
+from delorean import Delorean, parse
+import api_client
 import click
 import re
 
@@ -27,11 +29,13 @@ def cli(context, api_token, api_version):
 @click.option('--fqdn-pattern',
               help='Pattern for matching Fully Qualified Domain Names')
 @click.option('--tags', help='Comma-separated list of tags')
+@click.option('--start', help='Start time for the maintenance period. Default: now')
+@click.option('--end', help='End time for the maintenance period. Default: one minute from start')
 @click.pass_context
-def maintenance(context, customer_keys, dry_run, fqdn_pattern, tags):
+def maintenance(context, customer_keys, dry_run, fqdn_pattern, tags, start, end):
     client = context.find_object(api_client.api_client)
     if dry_run:
-        click.echo('DRY RUN')
+        click.secho('\nDRY RUN\n', bold=True)
 
     requests = []
     if customer_keys is not None:
@@ -49,15 +53,54 @@ def maintenance(context, customer_keys, dry_run, fqdn_pattern, tags):
         status_code = int(response['status_code'])
         if status_code == 200:
             servers.extend(response['response_data']['server_list'])
-        elif status_code == 401:
-            click.secho(response['status_reason'], fg='red')
-            if 'partner_customer_key' in response['status_reason']:
-                click.secho(query_params['partner_customer_key'], bold=True)
+        else:
+            raise click.ClickException('Response from API: "{}"'.format(response['status_reason']))
 
     if fqdn_pattern is not None:
         servers = [server for server in servers
                    if re.search(fqdn_pattern, server['fqdn']) is not None]
 
-    server_set = set([server['name'] for server in servers])
-    click.echo('\nMatching servers (' + str(len(server_set)) + '):')
-    click.echo(server_set)
+    start_time = parse(start) if start else Delorean()
+    end_time = parse(end) if end else start_time.next_minute()
+    duration = int(round((end_time - start_time).total_seconds() / 60))
+    if duration < 0:
+        raise click.BadOptionUsage(
+            start,
+            'Start time ({}) must occur before end time ({})'.format(start_time.datetime, end_time.datetime)
+        )
+    elif duration < 1:
+        raise click.BadOptionUsage(
+            end,
+            'End time ({}) must be at least one minute after start time ({})'.format(
+                end_time.datetime, start_time.datetime)
+        )
+
+    click.echo('Creating scheduled maintenance from {} to {} ({} minute{}) for {} servers:'.format(
+        start_time.datetime.strftime('%c'),
+        end_time.datetime.strftime('%c'),
+        duration,
+        's' if duration > 1 else '',
+        str(len(servers)))
+    )
+    # `server['url'].split('/')[-1]` gets the server's id
+    server_names = ['{} ({})'.format(server['name'], server['url'].split('/')[-1]) for server in servers]
+    server_names.sort()
+    for name in server_names:
+        click.echo(' * {}'.format(name))
+
+    if dry_run:
+        return
+
+    request_data = {
+        'description': 'Created using Panopta CLI',
+        'duration': duration,
+        'name': 'Scheduled Maintenance',
+        'original_start_time': start_time.datetime.isoformat(),
+        'targets': [server['url'] for server in servers],
+    }
+    response = client.post('maintenance_schedule', request_data)
+    if int(response['status_code']) == 201:
+        maintenance_schedule_id = response['response_headers']['id']
+        click.echo('Successfully created maintenance schedule {}'.format(maintenance_schedule_id))
+    else:
+        raise click.ClickException('Response from API: "{}"'.format(response['status_reason']))
--- panopta-cli/setup.py
+++ panopta-cli/setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
 
-setup(name='panopta',
-      version='0.1.0',
-      py_modules=['panopta'],
-      install_requires=['click', 'panopta_rest_api', 'watching-testrunner'],
-      entry_points='''
-        [console_scripts]
-        panopta=panopta:cli
-      ''')
+setup(
+    name='panopta',
+    version='0.1.0',
+    py_modules=['panopta'],
+    install_requires=[
+        'click',
+        'Delorean',
+        'panopta_rest_api',
+    ],
+    dependency_links=[
+        'git://github.com/Panopta/python-panopta-api-client.git@transfer-to-its-own-repo#egg=panopta_rest_api'
+    ],
+    entry_points={'console_scripts': ['panopta=panopta:cli']},
+)
